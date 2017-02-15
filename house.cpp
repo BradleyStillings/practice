9e6ad8c5eef57f7852e7a85c81b7c3bcf56b6377
BradleyStillings <bjstillings@yahoo.com>
2017-02-14 23:30:29 -0600

    Add files via upload

---
+++ house.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+  cout<<"#              /  \\                       # "<<endl;  
+  cout<<"#             /    \\                      #"<<endl; 
+  cout<<"#            /      \\                     #"<<endl;  
+  cout<<"#           /        \\                    #"<<endl;  
+  cout<<"#           **********                     #"<<endl;            
+  cout<<"#           *        *                     #"<<endl;             
+  cout<<"#           *  |  |  *                     #"<<endl;
+  cout<<"#           *  |  |  *                     #"<<endl;      
+
+  return 0;
+  }
+ 
